f3c558a975d6d740fe4fed1de9c1e9768a506adc
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2025-02-05 23:32:17 +0900

    refactor: wrap App component with InspireProvider and remove global styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { InspireProvider } from '@tapie-kr/inspire-react/provider';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <InspireProvider>
+      <App />
+    </InspireProvider>
   </StrictMode>,
-)
+);
