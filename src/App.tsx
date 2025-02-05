97fd83da2fcb0d7807a2ea033fc0187d073c18b8
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2025-02-06 00:40:53 +0900

    style: format App component for improved readability

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import { useState } from "react"
-import Header from "./components/Header"
-import RollingPaperModal from "./components/Modal/RollingPaperModal"
+import { useState } from 'react';
+import Header from './components/Header';
+import RollingPaperModal from './components/Modal/RollingPaperModal';
 
 function App() {
-  const [isModalVisible, setIsModalVisible] = useState(true)
+  const [isModalVisible, setIsModalVisible] = useState(true);
   return (
     <>
       <Header />
-      <RollingPaperModal visible={isModalVisible} onClose={() => setIsModalVisible(false)}></RollingPaperModal>
+      <RollingPaperModal
+        visible={isModalVisible}
+        onClose={() => setIsModalVisible(false)}></RollingPaperModal>
     </>
-  )
+  );
 }
 
 export default App;
