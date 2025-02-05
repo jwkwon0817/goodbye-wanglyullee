b42cd6a658f4b2fff216abd48c9e911244d456a8
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2025-02-06 00:33:33 +0900

    feat: enhance color theme structure with surface and shadow variables

--- src/styles/themes/color.css.ts
+++ src/styles/themes/color.css.ts
@@ -4,15 +4,31 @@ import {
 } from '@vanilla-extract/css';
 
 export const pastelVars = createGlobalThemeContract({
-  pink: 'pastel-pink',
-  blue: 'pastel-blue',
-  green: 'pastel-green',
-  yellow: 'pastel-yellow',
+  surface: {
+    pink: 'pastel-pink',
+    blue: 'pastel-blue',
+    green: 'pastel-green',
+    yellow: 'pastel-yellow',
+  },
+  shadow: {
+    pink: 'pastel-pink-shadow',
+    blue: 'pastel-blue-shadow',
+    green: 'pastel-green-shadow',
+    yellow: 'pastel-yellow-shadow',
+  },
 });
 
 createGlobalTheme(':root', pastelVars, {
-  pink: '#F54C7C',
-  blue: '#376BE7',
-  green: '#2DDA67',
-  yellow: '#EFBB00',
+  surface: {
+    pink: '#F54C7C',
+    blue: '#376BE7',
+    green: '#2DDA67',
+    yellow: '#EFBB00',
+  },
+  shadow: {
+    pink: '#D53361',
+    blue: '#305DCA',
+    green: '#2EC661',
+    yellow: '#C8A834',
+  },
 });
