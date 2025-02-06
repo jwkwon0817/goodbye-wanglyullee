9226c5de994546c0dc3f6e0571a5af0a10711d39
FIRO <72495729+iamfiro@users.noreply.github.com>
2025-02-06 13:37:05 +0900

    feat: implement dynamic browser height adjustment for Snowfall component

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,21 @@ import Background from './components/Background';
 import Header from './components/Header';
 import LetterBoxList from './components/LetterBoxList';
 import RollingPaperModal from './components/Modal/RollingPaperModal';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [browserHeight, setBrowserHeight] = useState(document.body.scrollWidth);
+
+  useEffect(() => {
+    console.log(browserHeight)
+    const handleResize = () => {
+      setBrowserHeight(document.body.scrollWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <>
       <Background>
@@ -14,7 +27,12 @@ function App() {
           <LetterBoxList />
         </HStack>
         <RollingPaperModal />
-        <Snowfall />
+        <Snowfall style={{
+          width: '100%',
+          height: browserHeight,
+        }}
+        snowflakeCount={700} 
+        />
       </Background>
     </>
   );
